8570f086f160435142b55878a1c9d0cefc8ecf0a
juancamilo399 <juan.angel-h@mail.escuelaing.edu.co>
2021-02-13 15:02:03 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import logo from './components/logo.svg';
+import './components/App.css';
+import { Login } from './components/Login';
+import Swal from 'sweetalert2'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  localStorage.setItem("Username", "juan@gmail.com");
+  localStorage.setItem("Password", "pass");
+  //localStorage.setItem("isLoggedIn","false")
+
+  let isLogged = localStorage.getItem("isLoggedIn");
+
+  isLogged = (isLogged === "true" ? true : false)
+
+  const [isLoggedIn, setisLoggedIn] = useState(isLogged)
+
+
+  const handleSuccessfullyLogin = (e) => {
+    Swal.fire({
+      title: 'Login succesfull',
+      text: 'Welcome',
+      timer: 2000,
+      timerProgressBar: false,
+      icon: 'success',
+      showConfirmButton: false
+    })
+    localStorage.setItem("isLoggedIn", "true");
+    setisLoggedIn(true)
+  }
+
+  const handleFailedLogin = (e) => {
+    Swal.fire({
+      title: 'Error!',
+      text: 'User or Password incorrect',
+      timer: 2000,
+      timerProgressBar: false,
+      icon: 'error',
+      showConfirmButton: false
+    })
+    localStorage.setItem("isLoggedIn", "false");
+    setisLoggedIn(false)
+  }
+
+  const LoginView = () => (
+    <Login successful={handleSuccessfullyLogin} failed={handleFailedLogin} />
   );
+
+  
+
+
+  
+
+  return (
+    <div>
+
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">TODO React App</h1>
+          </header>
+
+          <br />
+          <br />
+
+          <div>
+
+            <Route path="/" component={LoginView} />
+
+          </div>
+        </div>
+      </Router>
+
+    </div>
+  )
 }
 
 export default App;
